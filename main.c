62b88a33d798aabb4a6ad3ca43cf402629ce041a
Nathan Reed <vemhira@gmail.com>
2024-12-05 07:23:49 -0600

    feat: add stdin/pipe support

--- main.c
+++ main.c
@@ -2,16 +2,23 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define DIGITS 10
 #define STDIN_FILENO 0
 
 enum sig_state {
-    keyed_interrupt = 0,
-    keep_going = 1,
+    KeyedInterrupt = 0,
+    KeepGoing = 1,
 };
 
-struct content_stats {
+enum OP {
+    Manual,
+    FIO,
+};
+
+// content statistics
+struct cstats {
     int nwhite;
     int ntab;
     int nspace;
@@ -20,8 +27,8 @@ struct content_stats {
     int ndigit[DIGITS];
 };
 
-struct content_stats new_stats() {
-    struct content_stats s = {0,0};
+struct cstats new_stats() {
+    struct cstats s = {0,0};
 
     int i;
     for (i = 0; i < DIGITS; ++i) { s.ndigit[i] = 0; }
@@ -29,14 +36,14 @@ struct content_stats new_stats() {
     return s;
 }
 
-void inc_other(struct content_stats *s) {
+void inc_other(struct cstats *s) {
     s->nother++;
 }
 
-void inc_whitespace(struct content_stats *s) {
+void inc_whitespace(struct cstats *s) {
     s->nwhite++;
 }
-void inc_digit(struct content_stats *s, char c) {
+void inc_digit(struct cstats *s, char c) {
     s->ndigit[c-'0']++;
 }
 
@@ -44,29 +51,34 @@ void keyboard_interrupt(int);
 void nonfile_routine();
 void file_routine(FILE *f);
 
-enum sig_state sig = keep_going;
+enum sig_state sig = KeepGoing;
 
 int index_argv(char *term, int argc, char** argv) {
     int i;
     for (i = 0; i < argc; ++i) {
-        if (!strcmp(argv[i], term)) continue;
+        if (strcmp(argv[i], term) != 0) continue;
         return i;
     }
 
     return -1;
 }
+void print_help() {
+    printf("OPTIONS: [FILE|PIPE|STDIN]\n");
+}
 
 int main(int argc, char** argv) {
     if (argc <= 1) {
+        if (isatty(fileno(stdin)) == 0) {
+            file_routine(stdin);
+            return 0;
+        }
         return 0;
     }
 
-    int ret = index_argv("-p", argc, argv);
+    int ret = index_argv("-h", argc, argv);
     if (ret != -1) {
-        if ((ret+1) > argc-1) {
-            printf("contents: no path to file");
-            return 1;
-        }
+        print_help();
+        return 0;
     }
 
     --argc;
@@ -87,66 +99,64 @@ int main(int argc, char** argv) {
     }
 
     file_routine(fptr);
-
     return 0;
 }
 
 void keyboard_interrupt(int s) {
     extern enum sig_state sig;
-    sig = keyed_interrupt;
+    sig = KeyedInterrupt;
+}
+int fetch_char(enum OP o, FILE* f) {
+    int c;
+    if (o == Manual ) {
+        c = getchar();
+    } else {
+        if (f == NULL){ return EOF; }
+        c = fgetc(f);
+    }
+
+    return c;
 }
 
-void file_routine(FILE *f) {
+void fill_stats(enum OP o, FILE *f, struct cstats*s) {
     int i, c;
-    extern enum sig_state sig;
-    struct content_stats s = new_stats();
 
     do {
         if (c >= '0' && c <= '9') {
-            inc_digit(&s, c);
+            inc_digit(s, c);
         }
 
         if (c == '\t' || c == ' ') {
-            inc_whitespace(&s);
+            inc_whitespace(s);
             continue;
         }
 
-        inc_other(&s);
-    } while ((c = fgetc(f)) != EOF);
+        inc_other(s);
+    } while ((c = fetch_char(o, f)) != EOF);
+}
+
+void file_routine(FILE *f) {
+    extern enum sig_state sig;
+    struct cstats s = new_stats();
+    fill_stats(FIO, f, &s);
 
     printf("digits: \n");
-    for (i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; ++i) {
         printf("\t%d\t  ->  %d\n", i, s.ndigit[i]);
     }
 
     printf("\nwhitespace: %d\n", s.nwhite);
     printf("other: %d\n", s.nother);
+    fflush(stdout);
 }
 
 void nonfile_routine() {
     int i, c;
     extern enum sig_state sig;
-    struct content_stats s = new_stats();
-
-    // catch keyboard interrupt;
+    struct cstats s = new_stats();
     signal(SIGINT, keyboard_interrupt);
-
-    do {
-        if (sig == keyed_interrupt) {
-            break;
-        }
-
-        if (c >= '0' && c <= '9') {
-            inc_digit(&s, c);
-        }
-
-        if (c == '\t' || c == ' ') {
-            inc_whitespace(&s);
-            continue;
-        }
-
-        inc_other(&s);
-    } while ((c = getchar()) != '\v');
+    fill_stats(FIO, NULL, &s);
+    // catch keyboard interrupt;
 
     printf("digits: \n");
     for (i = 0; i < 10; ++i) {
